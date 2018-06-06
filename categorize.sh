d7a449b1c26e59d6b70f87296df57ad6449b8b56
gaiyu <gaiyu@geeker-4.news.cs.nyu.edu>
2018-06-06 16:53:28 +0000

    commit

---
+++ categorize.sh
@@ -0,0 +1,5 @@
+mkdir none sqrt linear quadratic
+mv *iw--* none
+mv *iw-sqrt* sqrt
+mv *iw-linear* linear
+mv *iw-quadratic* quadratic
--- run-tensorboards.sh
+++ run-tensorboards.sh
@@ -1,13 +1,18 @@
 port=$2
 
-for std in 0.01 0.1 1.0
+for y in ${1}/*
 do
-    for iw in none sqrt linear quadratic
-    do
-#       echo inspect/${1}-std-${std}/$iw
-#       echo $port
-        tensorboard --logdir=inspect/${1}-std-${std}/$iw --port=$port &
-        port=$((port + 1))
-    done
+    tensorboard --logdir=$y --port=$port &
+    port=$((port + 1))
 done
 wait
+
+# for x in log/${1}/processed/*
+# do
+#     for y in ${x}/*
+#     do
+#         tensorboard --logdir=$y --port=$port &
+#         port=$((port + 1))
+#     done
+# done
+# wait
