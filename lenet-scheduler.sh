39f834b09ce36d7fae3ebcfaa9641e8d33f28f8c
gaiyu <gaiyu@geeker-4.news.cs.nyu.edu>
2018-06-07 17:00:01 +0000

    commit

--- lenet-scheduler.sh
+++ lenet-scheduler.sh
@@ -3,18 +3,7 @@ iw_arr=("" sqrt linear quadratic)
 gpu=0
 for iw in "${iw_arr[@]}"
 do
-    ipython3 $1 -- --gpu=$gpu --iw=$iw --n-iterations-critic=1 &
-    ipython3 $1 -- --gpu=$gpu --iw=$iw --n-iterations-critic=5 &
-    gpu=$((gpu + 1))
-done
-wait
-
-gpu=0
-for iw in "${iw_arr[@]}"
-do
-    ipython3 $1 -- --gpu=$gpu --iw=$iw --n-iterations-critic=10 &
-    ipython3 $1 -- --gpu=$gpu --iw=$iw --n-iterations-critic=15 &
-    ipython3 $1 -- --gpu=$gpu --iw=$iw --n-iterations-critic=20 &
+    ipython3 $1 -- --gpu=$gpu --iw=$iw &
     gpu=$((gpu + 1))
 done
 wait
