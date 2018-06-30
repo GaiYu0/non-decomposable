12ec345e21cb41848cffcaf1960db7f3e6b27527
gaiyu <gaiyu@geeker-4.news.cs.nyu.edu>
2018-06-30 16:50:03 +0000

    commit

--- scheduler.sh
+++ scheduler.sh
@@ -3,7 +3,7 @@ iw_arr=(none sqrt linear quadratic)
 gpu=0
 for iw in "${iw_arr[@]}"
 do
-    ipython3 $1 -- --gpu=$gpu --iw=$iw &
+    ipython3 $1 -- --gpu $gpu --iw $iw &
     gpu=$((gpu + 1))
 done
 wait
