63660acce1238e189cd284ef949aa36d3b70a5de
gaiyu <gaiyu@geeker-4.news.cs.nyu.edu>
2018-07-01 13:27:26 +0000

    commit

--- es.py
+++ es.py
@@ -8,16 +8,16 @@ class GuidedES:
         self.g_list = []
 
     def perturb(self, module):
-        if len(self.g_list) < self.k or any(p.grad is None for p in module.parameters()):
-            my.perturb(module, self.std)
+        std, alpha, n, k = self.std, self.alpha, self.n, self.k 
+        if len(self.g_list) < k or any(p.grad is None for p in module.parameters()):
+            my.perturb(module, std)
         else:
-            std, alpha, n, k = self.std, self.alpha, self.n, self.k 
             self.g_list = self.g_list[1:]
             self.g_list.append(th.cat([p.grad.view(-1, 1) for p in module.parameters()]))
             u, _ = th.qr(th.cat(self.g_list, 1))
             g = std * (alpha / n) ** 0.5 * th.randn(n, 1) + \
-                    std * ((1 - alpha) / k) * th.mm(u, th.randn(k, 1))
+                    std * ((1 - alpha) / k) ** 0.5 * th.mm(u, th.randn(k, 1))
             i = 0
             for p in module.parameters():
-                p.data = g[i : i + p.grad.nelem()].view(p.data.shape)
+                p.data += g[i : i + p.grad.nelem()].view(p.data.shape)
                 i += p.data.numel()
